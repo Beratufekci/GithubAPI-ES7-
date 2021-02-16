c83fb9f5a45657a3ad4886ad7d1333d3b4412114
DESKTOP-0FBHRAR\DELL <berat.tufekci@kafein.com.tr>
2021-02-16 11:32:25 +0300

    UI Design

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const nameInput = document.getElementById("githubname");
 const clearLastUsers = document.getElementById("clear-last-users");
 const lastUsers = document.getElementById("last-users");
 const github = new Github();
+const ui = new UI();
 
 eventListeners();
 
@@ -24,14 +25,14 @@ function getData(e){
                 if(response.user.message === "Not Found"){
                     console.log("Err");
                 }else{
-                    console.log(response);
+                    ui.showUserInfo(response.user);
                 }
             })
             .catch(err => console.log(err));
     }
 
 
-
+    ui.clearInput();
     e.preventDefault();
 }
 
